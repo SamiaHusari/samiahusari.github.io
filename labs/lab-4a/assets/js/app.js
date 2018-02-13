bdc0f8e53ce8eb983729583590a2de07c700b354
SamiaHusari <samiahusari@gmail.com>
2018-02-13 13:12:30 -0500

    in class labs

---
+++ labs/lab-4a/assets/js/app.js
@@ -0,0 +1,17 @@
+// var first_name = 'Brian',
+//     last_name  = 'Watterson',
+//     full_name  = first_name + ' ' + last_name;
+
+// function hello(){
+//   alert(full_name);
+// }
+
+// hello()
+
+
+function hello(name, age) {
+  console.log('name', name);
+  console.log('not his real age', age);
+}
+
+hello ('Brian Watterson', 17);
